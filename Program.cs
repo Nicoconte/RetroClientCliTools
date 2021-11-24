bf991355ec9e1795178f0b800d5048e48b2041ea
Nicolas Conte <nicoconte1999@gmail.com>
2021-11-24 01:07:54 -0300

    CommandLineParse installed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
+using RetroClient.Data;
+using RetroClient.Services;
+
 namespace RetroClientCliTools
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
 		}
 	}
 }
